30c7fcb7e31c03bfabecf95693a7d4de528a99fd
Sjoao4018 <joao4018@gmail.com>
2017-04-03 19:12:23 -0300

    Inclusao inimigo.cpp,inimigo.hpp

---
+++ inimigo.cpp
@@ -0,0 +1,18 @@
+#include "inimigo.hpp"
+#include <iostream>
+
+using namespace std;
+
+Inimigo::Inimigo(int posicaoX,int posicaoY){
+
+	setDano(1);
+	setTipo('#');
+}
+
+int Inimigo::getDano(){
+	return dano;
+}
+
+void Inimigo::setDano(int dano){
+	this->dano = dano;
+}
---
+++ inimigo.hpp
@@ -0,0 +1,22 @@
+#ifndef INIMIGO_HPP
+#define INIMIGO_HPP
+
+#include "objetodejogo.hpp"
+#include <iostream>
+
+class Inimigo : public ObjetoDeJogo {
+
+private:
+	int dano;
+
+public:
+	Inimigo();
+	Inimigo(int posicaoX,int posicaoY);
+	~Inimigo();
+
+	void setDano(int dano);
+	int getDano();
+};
+
+
+#endif
